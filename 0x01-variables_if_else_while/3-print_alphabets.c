b489656a12359280b56f0be834ab20264b512ba5
idyweb <inyangidongesit22@gmail.com>
2023-03-20 11:38:41 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a function that prints alphabets in lower and uppercase.
+ * Return: (0) Sucess)
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
+
