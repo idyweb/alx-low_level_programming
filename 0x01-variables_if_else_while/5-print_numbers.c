0a81a77b949f79d393d5b2e60fd8ceddb365a68e
idyweb <inyangidongesit22@gmail.com>
2023-03-21 02:19:42 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints numbers in a range of 10.
+ * Return: (0) as success
+ *
+ */
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+
+		printf("%d\n", x);
+
+	return (0);
+}
