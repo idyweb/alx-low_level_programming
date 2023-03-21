10c08b1034ad829058668b2593c9d05935e9ba85
idyweb <inyangidongesit22@gmail.com>
2023-03-21 03:33:32 +0100

    print_ comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio>
+/**
+ * main - prints all posssible combination of two digits.
+ *
+ * Return: (0) Success
+ */
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x < 100; x++)
+	{
+		j = x + 1;
+		while (j < 100)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (x < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
