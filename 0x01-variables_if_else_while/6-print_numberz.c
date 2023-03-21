7389bcfaec466982958138cd57647151913b17ea
idyweb <inyangidongesit22@gmail.com>
2023-03-21 02:30:58 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function that prints all single digits.
+ * Return: (0) as success
+ *
+ */
+int main(void)
+
+{
+	char x;
+
+	for (x = '0'; x < 10; x++)
+		putchar(x);
+	putchar("\n");
+	return (0);
+}
