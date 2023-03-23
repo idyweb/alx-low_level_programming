6bfb48fa7a0efb87bc0202af137ea281b471e6fc
idyweb <inyangidongesit22@gmail.com>
2023-03-23 20:26:00 +0100

    debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Test function that prints if integer is positive or nrgative
+ * Return: 0
+ */
+int main(void)
+
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-ALX low level programming
+This is the readme for FUNCTIONS NESTED LOOPS.
