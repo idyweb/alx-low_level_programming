62da835fa30bbf33225add4e153414a1c6ff7bdb
idyweb <inyangidongesit22@gmail.com>
2023-03-31 03:15:55 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,35 @@
 
 /**
  * *_strcat - function that appends two strings.
+ *@dest : input value
+ * @src: input value
  *
- *
- * Return - Always 0.
+ * Return:  void
  */
 
 char *_strcat(char *dest, char *src)
 
 {
-	char *p = dest;
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
 
