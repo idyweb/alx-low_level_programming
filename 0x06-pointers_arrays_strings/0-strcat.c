ae4b58e014de0e8cad2018c56d41914fbed01122
idyweb <inyangidongesit22@gmail.com>
2023-03-31 03:08:25 +0100

    main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - function that appends two strings.
+ *
+ *
+ * Return - Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	char *p = dest;
+
