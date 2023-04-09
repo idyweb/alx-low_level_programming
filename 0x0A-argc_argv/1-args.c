37bc958194cbf6a4a85073e56c7885d669e3c172
idyweb <inyangidongesit22@gmail.com>
2023-04-09 17:01:31 +0100

    silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - program that prints the number of arguments
+ * passed to it
+ * @argc: number of arguments passed
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("this is the number of arguments sent %d\n", argc);
+	return (0);
+}
+
