d21acc655b901945565f7867b54e0c6fd5fc4b2e
idyweb <inyangidongesit22@gmail.com>
2023-04-09 11:51:02 +0100

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name
+ * @argc: count of argument
+ * @argv: Array of argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+
+{
+	printf("this is y name %s\n", argv[0]);
+
+	return (0);
+}
