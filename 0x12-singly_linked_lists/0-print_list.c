8169af62716b654ed3f2cef3b74b9ea33629505b
idyweb <inyangidongesit22@gmail.com>
2023-04-27 17:44:50 +0100

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	
+
 	while (h)
 	{
 		if (!h->str)
@@ -20,6 +20,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
-	
+
 	return (s);
 }
