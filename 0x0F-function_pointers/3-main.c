4798f83166f19dbdcdd1996533604197a7bbeb44
idyweb <inyangidongesit22@gmail.com>
2023-10-13 11:41:56 +0100

    latest commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  * Return: always 0.
  */
 
-int main(int__attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
