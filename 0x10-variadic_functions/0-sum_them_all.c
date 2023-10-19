4a897e409cb35d77bf4b636114ba49bcd800fa3e
idyweb <inyangidongesit22@gmail.com>
2023-10-19 13:39:35 +0100

    variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ ** sum_them_all - returns the sum of all its parameters.
+ ** @x: amount of the arguments.
+ **
+ ** Return: sum of its parameters.
+ **/
+int sum_them_all(const unsigned int x, ...)
+{
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (x == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,10 @@
+0x10. C - Variadic functions
+Learning Objectives
+General
+
+What are variadic functions
+How to use va_start, va_arg and va_end macros
+Why and how to use the const type qualifier
+Requirements
+All files are created and compiled on Ubuntu 14.04.4 LTS on gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
+All files are linted for syntax and style with Betty
